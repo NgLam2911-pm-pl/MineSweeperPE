e2dd37804b724cfc2593a895f47d09120d556dc7
ThinkerS2k18 <62464250+ThinkerS2k18@users.noreply.github.com>
2020-03-30 21:31:57 +0700

    Update StartMine.php
    I changed the messages in the game.

--- src/NgLamVN/minesweeper/command/StartMine.php
+++ src/NgLamVN/minesweeper/command/StartMine.php
@@ -23,33 +23,35 @@ class StartMine extends PluginCommand
     {
         if (!$sender->hasPermission("mspe.command.start"))
         {
-            $sender->sendMessage("U not have perm to use this command");
+            $sender->sendMessage("§f[§bMineSweeper§f]§c You don't have permission to use this command.");
             return;
         }
         if (!($sender instanceof Player))
         {
-            $sender->sendMessage("Please use this command in game");
+            $sender->sendMessage("§f[§bMineSweeper§f]§c Please use this command in game");
             return;
         }
         if (!isset($args[0]))
         {
-            $sender->sendMessage("/startmine <Max X> <Max Y> <Bombs>");
+            $sender->sendMessage("§f[§bMineSweeper§f]§a /startmine§e <Max X> <Max Y> <Mines>");
+            $sender->sendMessage("§f[§bMineSweeper§f]§a X & Y:§e Maximum width.");
+            $sender->sendMessage("§f[§bMineSweeper§f]§a Bombs:§e The maximum amounts of mine that contain in the field.");
             return;
         }
         if (!isset($args[1]))
         {
-            $sender->sendMessage("/startmine <Max X> <Max Y> <Bombs>");
+            $sender->sendMessage("§f[§bMineSweeper§f]§a /startmine <Max X> <Max Y> <Mines>");
             return;
         }
         if (!isset($args[2]))
         {
-            $sender->sendMessage("/startmine <Max X> <Max Y> <Bombs>");
+            $sender->sendMessage("§f[§bMineSweeper§f]§a /startmine <Max X> <Max Y> <Mines>");
             return;
         }
         if ($args[2] > (($args[0] * $args[1]) - 9))
         {
             $bombss = round((($args[0] * $args[1]) * (60 / 100)), 0, PHP_ROUND_HALF_DOWN);
-            $sender->sendMessage("Bomb must be lower than: " . $bombss);
+            $sender->sendMessage("§f[§bMineSweeper§f]§c Error! Mine must be lower than:§e " . $bombss . "§c mine(s)");
             return;
         }
         $this->plugin->game->startGame($args[0], $args[1], $args[2]);
